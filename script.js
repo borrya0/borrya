118074f1133f6be14f05b541f42a4a5a2ce2f3e9
borya <borya.vasnetsov@bk.ru>
2019-04-09 22:59:59 +0300

    Это наш перавый комит

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict'
+a = 5;
+console.log(a);
+
+// let arr = {
+//     name: 'Jhon',
+//     age: 21, 
+//     color: 'red'
+// };
+// console.log(arr["name"]);
+// console.log(arr["color"]);
+// let rra = ['one', 'two', 'three'];
+// console.log(rra[1]);
+// let dead = confirm('Вам есть восемнадцать?');
+// console.log(dead);
+
+// let rRa = ['one', 'two', 'three']
+//  console.log(rRa([1]));
+
+// let abc = +prompt("Вам есть 18?");
+// console.log(typeof(abc));
+// let incr = 10;
+//     decr = 10;
+//     incr++;
+//     decr--;
+//     console.log(incr);
+//     console.log(decr);
+let abc = true;
+    cba = false;
+console.log(abc && !cba);
+let def = false;
+    fed = false;
+console.log( !def || fed)
